44bc903ae0d87954ff5a4ff1a4db946e6f3473e5
Lovro Vrček <vrcek.lovro@gmail.com>
2021-05-24 17:44:24 +0800

    Updated gitignore

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # Create necessary directories
 mkdir -p data/train/raw data/train/processed
 mkdir -p data/test/raw data/test/processed
-mkdir -p data/reads
 mkdir trained_models
 mkdir figures
 
@@ -10,6 +9,5 @@ cd vendor/raven
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. && make
-
-# Back to the project directory
 cd ../../..
+
